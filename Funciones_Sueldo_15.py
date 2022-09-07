77c1f6dce4bd40068bf8767c630d05ee93ac2d5a
EGonzalez <egonzalez12@miumg.edu.gt>
2022-09-06 21:54:41 -0600

    Funciones

---
+++ Funciones_Sueldo_15.py
@@ -0,0 +1,7 @@
+def calcular_sueldo(nombre,costohora,cantidadhoras):
+    sueldo=costohora*cantidadhoras
+    print(nombre,"trabajo",cantidadhoras,"y cobra un sueldo de",sueldo)
+
+calcular_sueldo("esvin",12,220)
+calcular_sueldo(costohora=12,cantidadhoras=40,nombre="giovanni")
+calcular_sueldo(cantidadhoras=90,nombre="gonzalez",costohora=7)
